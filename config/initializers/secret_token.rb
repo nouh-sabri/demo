0fa64ad1cd1de3a2871c4b1f606ee003bacaab12
nouh sabri <nouh.sabri@gmail.com>
2011-10-05 19:33:17 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nouh::Application.config.secret_token = '51ccd08db7578eff0a78aa4e585e369fc17b6570d4b89f70b32c6b485e0f76691251a95758ae8bfbfced601ce9ac812ba3091a3a51b440262c051defc0d17533'
